38a30be66dc694d1747b987b17b35441e84c6d56
AlexandreSGJR <alexandressgjr@gmail.com>
2020-10-29 14:33:10 -0300

    Correção login

--- Algoritmo/Site Institucional/js/scriptCadastroLogin.js
+++ Algoritmo/Site Institucional/js/scriptCadastroLogin.js
@@ -2,10 +2,10 @@ function login() {
 
     if (usuario.value == 'frigologia@gmail.com' && senha.value == 'frigologia') {
         window.location.href = "../Dashboard/dashboard.html";
-
+        limpar();
     } else {
         alert("Senha ou usuário incorreto!");
-
+        limpar();
     }
 
 }
@@ -13,6 +13,7 @@ function login() {
 // setTimeout ele executa algo depois que o tempo determinado acabar
 // setTimout = ele pega o id, configura a propriedade style e depois configura a opacidade do style
 function abrir() {
+    limpar();
     popup.style.display = 'block';
     form.style.display = 'none';
     setTimeout(() => {
@@ -30,11 +31,16 @@ function fechar() {
     }, 200);
 }
 function cadastro() {
-   form.style.display = '1';
+    form.style.display = '1';
     popup.style.opacity = '0';
     setTimeout(() => {
         popup.style.display = 'none';
         form.style.display = 'block';
     }, 200);
     alert("usuario cadastrado com sucesso !!");
+}
+
+function limpar() {
+    usuario.value = '';
+    senha.value = '';
 }
