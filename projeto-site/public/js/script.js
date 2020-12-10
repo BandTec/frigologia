590dad51f08b59b86b656897aa22ad1744ff17a5
DavidRafaelDev <davidrafaelmsm3@gmail.com>
2020-12-09 21:10:20 -0300

    mudando o script do mudar membro

--- projeto-site/public/js/script.js
+++ projeto-site/public/js/script.js
@@ -4,10 +4,10 @@ function changeTeam() {
     let members = []; 
     let id_img_members = [];
     let id_name_members = [];
-    let imgs_members_sprint_1 = ['david', 'dennis', 'felipe','gabriel','jose','lucas','renato'];
-    let names_members_sprint_1 = ['David Rafael', 'Dennis Barbosa', 'Felipe Kling','Gabriel Alvares','José Fabiano','Lucas Ferreira','Renato Paulino'];
-    let imgs_members_sprint_2 = ['alexandre', 'arthur', 'christian','david','dennis','gabriel-aguiar'];
-    let names_members_sprint_2 = ['Alexandre Junior', 'Arthur Loureano', 'Christian Raines','David Rafael','Dennis Barbosa','Gabriel Aguiar'];
+    let imgs_members_sprint_1 = ['david','felipe','gabriel','jose','lucas','renato'];
+    let names_members_sprint_1 = ['David Rafael', 'Felipe Kling','Gabriel Alvares','José Fabiano','Lucas Ferreira','Renato Paulino'];
+    let imgs_members_sprint_2 = ['alexandre', 'arthur', 'christian','david'];
+    let names_members_sprint_2 = ['Alexandre Junior', 'Arthur Loureano', 'Christian Raines','David Rafael'];
     
     for (let i = 0; i <= 7; i++) {
         members.push(document.getElementById('member'+i));
@@ -19,11 +19,14 @@ function changeTeam() {
         
         /** modify the layout */
         members[7].style.display= 'flex'; 
+        members[6].style.display= 'flex'; 
+        members[5].style.display= 'flex'; 
+
         members[4].style.marginTop = '0px';
         document.getElementById('details-team').style.maxWidth= '1225px';
 
         /** Updating source of images and name of the members */
-        for(let i = 1, n = 0; i <= 7; i++, n++) {
+        for(let i = 1, n = 0; i <= 6; i++, n++) {
             id_img_members[i].src = 'img/team-members/'+imgs_members_sprint_1[n]+'.jpeg';
             id_name_members[i].innerHTML = names_members_sprint_1[n];
         }
@@ -31,11 +34,13 @@ function changeTeam() {
 
         /** modify the layout */
         members[7].style.display= 'none'; 
+        members[6].style.display = 'none';
+        members[5].style.display = 'none';
         members[4].style.marginTop = '15px';
         document.getElementById('details-team').style.maxWidth= '900px';
 
         /** Updating source of images and name of the members */
-        for(let i = 1, n = 0; i <= 7; i++, n++) {
+        for(let i = 1, n = 0; i <= 4; i++, n++) {
             id_img_members[i].src = 'img/team-members/'+imgs_members_sprint_2[n]+'.jpeg';
             id_name_members[i].innerHTML = names_members_sprint_2[n];
         }
